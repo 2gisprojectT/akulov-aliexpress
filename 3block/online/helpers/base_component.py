6d1cbd041ece4b6981bd683383f722279f2b9fe2
DjonnyD <robocul@mail.ru>
2014-10-23 12:42:04 +0600

    Revert "ДЗ по 2GIS"
    This reverts commit 5853770f37500f7b7b9afdd6d1648a28acc05b1f.

--- 3block/online/helpers/base_component.py
+++
@@ -1,8 +0,0 @@
-class BaseComponent(object):
-    def __init__(self, driver, element=None):
-        """
-        :type driver: WebDriver
-        :type element: WebElement
-        """
-        self.driver = driver
-        self.element = element
